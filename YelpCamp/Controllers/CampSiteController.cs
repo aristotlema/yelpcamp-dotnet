3862314577d7b70f37b3a3a714477beb2c14b138
Aristotle Maule <aristotle.maule@hotmail.com>
2021-03-21 11:22:09 -0400

    Create route, some styling and added required feilds and validation

--- YelpCamp/Controllers/CampSiteController.cs
+++ YelpCamp/Controllers/CampSiteController.cs
@@ -3,14 +3,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YelpCamp.Data;
+using YelpCamp.Models;
 
 namespace YelpCamp.Controllers
 {
     public class CampSiteController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CampSiteController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            IEnumerable<CampSite> objList = _db.Campsite;
+            return View(objList);
+        }
+
+        // GET - Create
+        public IActionResult Create()
         {
             return View();
         }
+
+        // POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CampSite obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Campsite.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
     }
 }
--- YelpCamp/Models/CampSite.cs
+++ YelpCamp/Models/CampSite.cs
@@ -10,8 +10,11 @@ namespace YelpCamp.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
